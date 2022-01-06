9de0ddf13248781e7f24562b888d9ca715f67a65
Arvindsamraj <arvindsamraj@gmail.com>
2022-01-06 17:27:20 +0530

    weather app first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = 'Uj9RdxvTUWGxo6PkUPJIiEgHJbWtUBcs';
+
+//get weather info
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+
+//get city info
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// getCity('delhi')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
